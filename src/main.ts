f14970abd134e2d3a8ac8f24acff01ab0a141742
Alexis Argyris <alexis.argyris@gmail.com>
2018-12-31 11:39:56 +0200

    issue-2

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
-import {Aurelia} from 'aurelia-framework'
-import environment from './environment';
+import { Aurelia } from 'aurelia-framework'
+import environment from './environment'
 
 export function configure(aurelia: Aurelia) {
-  aurelia.use
-    .standardConfiguration()
-    .feature('resources');
+  aurelia.use.standardConfiguration().feature('resources')
 
-  aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
+  aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn')
 
-  if (environment.testing) {
-    aurelia.use.plugin('aurelia-testing');
-  }
-
-  return aurelia.start().then(() => aurelia.setRoot());
+  requirejs.config({ paths: { vs: '../scripts/vs' } })
+  return aurelia.start().then(() => aurelia.setRoot())
 }
--- src/resources/elements/monaco-editor/monaco-editor.ts
+++ src/resources/elements/monaco-editor/monaco-editor.ts
@@ -4,8 +4,12 @@ import { EditorFactory } from './editor-factory'
 @autoinject
 export class MonacoEditor {
   public editorHost: HTMLElement
+  private editorHostContainer: any
   private editorFactory: EditorFactory
   private editor: any
+  private resizeTimer = null
+  private readonly throttleLimit: number = 250
+  public readonly paddingHeight: number = 32
 
   constructor(editorFactory: EditorFactory) {
     this.editorFactory = editorFactory
@@ -22,6 +26,21 @@ export class MonacoEditor {
       })
       .then(ed => {
         this.editor = ed
+        this.editorHostContainer = this.editorHost.parentNode.parentNode
+        this.onResized()
+        window.addEventListener('resize', this.onResized.bind(this))
       })
+  }
+  public onResized() {
+    // adjust the editor's height to its container's height
+    // use throttling (https://stackoverflow.com/questions/35937020/aurelia-resize-layout-change-event-for-view)
+    clearTimeout(this.resizeTimer)
+    this.resizeTimer = setTimeout(() => {
+      this.editorHost.style.height =
+        (
+          this.editorHostContainer.clientHeight - this.paddingHeight
+        ).toString() + 'px'
+      this.editor.layout()
+    }, this.throttleLimit)
   }
 }
